b07146de341f7d8eb075aaf283c4dc73b61e9bee
segerpeter07 <peter.seger@students.olin.edu>
2016-12-01 17:13:24 -0500

    new SDD we made with Eric

--- HereWeGooo.m
+++ HereWeGooo.m
@@ -8,7 +8,7 @@ m = rolling_mass;                       %ez pz
 
 R = 100;         %m, radius of the roller coaster loop
 g = 9.81;       %m/s^2, acceleration due to gravity
-mu = 0.0009;    %coefficient of rolling resistance
+mu = 0.9009;    %coefficient of rolling resistance
 
 
     function res = S_derivs(~,S)
@@ -28,7 +28,9 @@ mu = 0.0009;    %coefficient of rolling resistance
 %           else
 %             sdd=(m*sdot*cos(s)*(R*tan(s)-mu*R*tan(s))-m*sdot^2*sin(s)+m*g)/(m*sin(s)*(R*tan(s)-mu*R*tan(s))-m*cos(s));
 %           end
-            sdd=(m*sdot^2*sin(s)-m*g-tan(s)*((1-mu)/(1+mu))*m*sdot^2*cos(s))/(m*cos(s)+m*sin(s)*tan(s)*((1-mu)/(1+mu)));
+            
+%             sdd=(m*sdot^2*sin(s)-m*g-tan(s)*((1-mu)/(1+mu))*m*sdot^2*cos(s))/(m*cos(s)+m*sin(s)*tan(s)*((1-mu)/(1+mu)));
+            sdd=(-sdot^2*cos(s)*sin(s)-mu*sign(sdot)*sdot^2*sin(s)*cos(s)+m*sdot^2*sin(s)*cos(s)-m*g*cos(s)+mu*sign(sdot)*sdot^2*cos(s)*sin(s)-mu*sign(sdot)*m*g*cos(s))/((sin(s)^2)+mu*sign(sdot)*sin(s)+m*(cos(s)^2)+mu*sign(sdot)*(cos(s)^2));
         res = [sdot; sdd];
     end
 
@@ -42,7 +44,7 @@ mu = 0.0009;    %coefficient of rolling resistance
 %     end
 
 options = odeset('RelTol', 1e-10);
-[t,S] = ode45(@S_derivs, [0 20], [pi,0],options);
+[t,S] = ode45(@S_derivs, [0 5], [pi,4],options);
 %>>>>>>> origin/master
 
 Ss = S(:,1);
